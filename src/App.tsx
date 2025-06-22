9b4c3e5983039f10e02f24f492dac194ca70d105
mohammd jehad <mhmddahamsheh@gmail.com>
2025-06-22 16:29:13 +0300

    make the navbar and the header of the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './layout/navbarAndFooter/Navbar';
+import { ExploerTopBooks } from './layout/HomePage/ExploerTopBooks';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <ExploerTopBooks/>
     </div>
   );
 }
---
+++ src/layout/HomePage/ExploerTopBooks.tsx
@@ -0,0 +1,15 @@
+export function ExploerTopBooks() {
+  return (
+    <header className="header bg-dark p-5 mb-4">
+      <div className="container-fluid py-3 text-white d-flex justify-content-center align-items-center">
+        <div>
+          <h1 className="fw-bold display-5">Find your next advenure</h1>
+          <div className="fs-4 col-md-8">where wolud you like go next</div>
+          <a className="btn main-color text-light btn-sm" href="#">
+            Exploer top books
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
